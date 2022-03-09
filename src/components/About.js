ca566bac90ece2263cd02805527e02cd8fb6eb32
Amber <martinez.amber1028@gmail.com>
2022-03-09 12:41:29 -0800

    Assignment completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Hi! I'm Amber Martinez and am currently a software engineering student at Flatiron School. I'll be graduating the program by the end of 2022 and am very excited to get into the industry! Feel free to connect with me. Always up for a chat!
+      </p>
+      <img src={image} alt='I made this'></img>
+    </div>
+  )
 }
 
 export default About;
